d318c675d69940df4cce9a69e718b02c31d571cf
Olabimpe Abesin <abesinolabimpe@gmail.com>
2025-05-07 20:52:05 +0100

    implemented hover state

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ function updateCartDisplay() {
       </div>
     </div>
       <button class="remove-item">
-        <img src="assets/images/icon-remove-item.svg" alt="delete icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path class="remove-icon" fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
       </button>
     `;
     addedItems.appendChild(addedItem);
